575a5caacde3004c1b2facb2624269db363b4b27
Sushikat <ikatbuschel@comcast.net>
2014-12-07 23:40:29 -0800

    finished through part 3

--- Homework11/src/Homework11.java
+++ Homework11/src/Homework11.java
@@ -4,5 +4,84 @@
     Homework 11 Graphs
     12/8/2014
 
-    
+Part I: B-Trees
+
+Draw the tree that results from adding the values 12, 24, 32, 42, 48, 56, 67, 83, 89, and 95 to an empty 2-3 tree.
+Draw the tree that results from adding these same values to an empty 2-4 tree.
+For #1 and #2, show what the tree looks like after each element is added. You do not need to show the splitting steps,
+but you must show what the tree looks like after each insertion.
+(There should be 10 trees in your answer to #1 and 10 trees in your answer to #2).
+See Part1AllAnswers.pdf
+
+Part II: Graphs
+
+For the three graphs below, describe each of the following:
+directed/undirected
+number of nodes
+number of edges
+unweighted/weighted
+connected/disconnected
+complete/not complete
+cyclic/acyclic
+a.
+directed/undirected: undirected
+number of nodes: 4
+number of edges: 3
+unweighted/weighted: unweighted
+connected/disconnected: disconnected
+complete/not complete: complete
+cyclic/acyclic: cyclic
+b.
+directed/undirected: undirected
+number of nodes: 6
+number of edges: 10
+unweighted/weighted: unweighted
+connected/disconnected: connected
+complete/not complete: complete
+cyclic/acyclic: cyclic
+c.
+directed/undirected: undirected
+number of nodes: 4
+number of edges: 5
+unweighted/weighted: unweighted
+connected/disconnected: connected
+complete/not complete: not complete
+cyclic/acyclic: cyclic
+
+Trace the traversal of the graph below using the type of traversal and the starting vertex below.
+(To be consistent, use alphabetic ordering for the order in which neighbors are visited.)
+breadth-first, starting at A
+A, D, J, G, B, F, E (never gets to C)
+breadth-first, starting at C
+C, B, E, H, D, A, J, G, E
+depth-first, starting at A
+A, D, J, G, B, F, E (H and C are never visited
+depth-first, starting at C
+C, B, D, J, G, F, E, H, A
+
+Part III: Chapter 24, Page 727 / Chapter 30 Page 816: 8, 11; Extra Credit: 16, EC
+
+#8 (the topological ordering)
+See Part3.pdf
+#11 (the map): for part b, provide two different sets of edges that could be removed to make the graph a tree.
+Extra Credit: #16: I recommend using the interactive map on the Jet Blue website: http://www.jetblue.com/WhereWeJet/.
+If you use this, use Buffalo, NY instead of Albany, NY.
+If you use a different airline, you must include the map or the URL in your answer.
+Extra Credit EC: Describe real-world situations that could be represented by a graph with the properties below.
+For each graph, describe what the nodes are and what an edge represents.
+directed and disconnected
+undirected and disconnected
+complete
+Part IV: Chapter 25, Page 750 / Chapter 31, Page 839: 1-4, 6-7, 17
+
+
+For the questions that ask you to draw a tree, you can include your answer in whatever way is easiest.
+You can draw the answer on paper and then scan the answer or take a picture of it with a
+phone - however you can capture it is fine! I recommend a free program called draw io: https://www.draw.io/‎.
+This is the program I use to create the trees for the online notes. I find it very user-friendly.
+
+However you create the pictures of your trees, make sure that the pictures are clearly labeled and organized.
+This means either naming each file with the chapter/exercise number or putting each picture into one main doc/pdf file.
+It's your responsibility to make sure it's easy for me/the graders to find your answers!
+
  */
